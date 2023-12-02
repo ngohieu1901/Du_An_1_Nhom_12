70f236f618eb4e962569d2108ee430bb236dcb6d
dhdhdhdhd <ngo3560@gmail.com>
2023-12-02 15:46:31 +0700

    update notifycation 1

--- app/src/main/java/com/example/du_an_1_nhom_12/Activity/MainActivity.java
+++ app/src/main/java/com/example/du_an_1_nhom_12/Activity/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.du_an_1_nhom_12.Activity;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -17,7 +14,6 @@ import android.preference.PreferenceManager;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -27,7 +23,7 @@ import com.example.du_an_1_nhom_12.SUPPORT.NotifyConfig;
 import java.util.Date;
 
 
-public class SplashActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
 
     @Override
@@ -51,38 +47,38 @@ public class SplashActivity extends AppCompatActivity {
             public void onFinish() {
                 if (lang_selected){
                     //dc chon
-                    Intent intent = new Intent(SplashActivity.this, MainViewPager.class);
+                    Intent intent = new Intent(MainActivity.this, MainViewPager.class);
                     startActivity(intent);
                     finishAffinity();
                     if (started) {
                         //dc chon
-                        Intent intent0 = new Intent(SplashActivity.this, PermissionActivity.class);
+                        Intent intent0 = new Intent(MainActivity.this, PermissionActivity.class);
                         intent0.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent0);
                         finishAffinity();
                         if (permission){
                             //dc chon
-                            Intent intent1 = new Intent(SplashActivity.this, ManageFileActivity.class);
+                            Intent intent1 = new Intent(MainActivity.this, ManageFileActivity.class);
                             intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent1);
                             finishAffinity();
                         }else {
                             //chua dc chon
-                            Intent intent2 = new Intent(SplashActivity.this,PermissionActivity.class);
+                            Intent intent2 = new Intent(MainActivity.this,PermissionActivity.class);
                             intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent2);
                             finishAffinity();
                         }
                     }else {
                         //chua dc chon
-                        Intent intent3 = new Intent(SplashActivity.this, MainViewPager.class);
+                        Intent intent3 = new Intent(MainActivity.this, MainViewPager.class);
                         intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent3);
                         finishAffinity();
                     }
                 }else {
                     //chua dc chon
-                    Intent intent4 = new Intent(SplashActivity.this, LanguageActivity.class);
+                    Intent intent4 = new Intent(MainActivity.this, LanguageActivity.class);
                     intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent4);
                     finishAffinity();
@@ -96,13 +92,13 @@ public class SplashActivity extends AppCompatActivity {
         Intent intendDemo = new Intent(this, TutorialActivity.class);
         intendDemo.putExtra("duLieu","Dữ liệu gửi từ notifi vao acti");
         //tạo stack để gọi acti
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(SplashActivity.this);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(MainActivity.this);
         stackBuilder.addNextIntentWithParentStack(intendDemo);
         //Lay pendingIntent de truyen vao notifi
         PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_IMMUTABLE);
         //Khoi tao layout cho Notify
         Bitmap logo = BitmapFactory.decodeResource(getResources(),R.drawable.bg_notify);
-        Notification customNotification = new NotificationCompat.Builder(SplashActivity.this, NotifyConfig.CHANEL_ID)
+        Notification customNotification = new NotificationCompat.Builder(MainActivity.this, NotifyConfig.CHANEL_ID)
                 .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
                 .setContentTitle("Tips and Tricks use PDF Manager")
                 .setContentIntent(resultPendingIntent)//intend nhan tuong tac
@@ -116,7 +112,7 @@ public class SplashActivity extends AppCompatActivity {
                 .setAutoCancel(true)
                 .build();
         //Khoi tao manager de quan notifi
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(SplashActivity.this);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
         // moi lan hien thi notifi can tao 1 cai ID cho thong bao rieng
         int id_notify = (int) new Date().getTime();// lay chuoi time la phu hop
         //lenh hien thi notify
